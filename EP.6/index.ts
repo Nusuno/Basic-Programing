f3d210346488e9d33f39da88029f4ae21552ee0f
Nusuno <nuskung2547@gmail.com>
2024-08-30 09:46:16 +0700

    EP.6

---
+++ EP.6/index.ts
@@ -0,0 +1,11 @@
+// const sName: string = 'n1, n2, n3, n4, n5, n6'
+
+// const sNameArray = sName.split(',')
+// console.log(sNameArray.join('#'))
+
+
+const aName: string = 'dog*cat*tiger*lion'
+const aNameArray = aName.split('*')
+aNameArray.pop()
+aNameArray.push('cow')
+console.log(aNameArray)
