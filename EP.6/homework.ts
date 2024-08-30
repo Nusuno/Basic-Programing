7b5c3cd4c248748e19a15b98ec94b372e5357e07
Nusuno <nuskung2547@gmail.com>
2024-08-30 11:19:18 +0700

    Create homework.ts

---
+++ EP.6/homework.ts
@@ -0,0 +1,13 @@
+const personalInformation = {
+    fullName: 'นายสมมุติ สุดหล่อ',
+    address: '123/4',
+    moo: '5',
+    road: 'ลื่นไหล',
+    Subdistrict: 'น้ำตก',
+    district: 'อัมพรรณ',
+    province: 'กำแพงเพชร',
+    zipCode: '11101001',
+    telePhone: '0987654321',
+    phone: '0987654321',
+    email: 'timestone@gmail.com',
+}
