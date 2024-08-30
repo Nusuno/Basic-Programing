a4cadcb634357abbd69f4fcac34be34828b722d0
Nusuno <nuskung2547@gmail.com>
2024-08-30 10:03:06 +0700

    Update index.ts

--- EP.6/index.ts
+++ EP.6/index.ts
@@ -1,12 +1,13 @@
 // const sName: string = 'n1, n2, n3, n4, n5, n6'
 
-// const sNameArray = sName.split(',')
-// console.log(sNameArray.join('#'))
+// const sNameArray = sName.split(',') //.split คือการแยกข้อมูลให้ออกจากกัน เปลี่ยนเป็น Array
+// console.log(sNameArray.join('#')) //.join คือการใส่ตัวอะไรก็ได้ให้มันมาเว้นใส่ในข้อมูล กลับเป็น string
 
 
-const aName: string = 'dog*cat*tiger*lion'
+const aName: string = 'dog*cat*bat*lion'
 const aNameArray = aName.split('*')
-aNameArray.pop()
-aNameArray.push('cow')
+aNameArray.pop() //ดึงข้อมูลตัวสุดท้ายออก
+aNameArray.shift() //ดึงข้อมูลตัวแรกออก
+aNameArray.push('cow') //ใส่ข้อมูลแทนตัวสุดท้าย
+aNameArray.unshift('rat') //ใส่ข้อมูลแทนตัวแรก
 console.log(aNameArray)
-
