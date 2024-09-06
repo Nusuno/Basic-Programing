b4f9d851f0d1ffce16c1280642e65aedebcd045b
Nusuno <nuskung2547@gmail.com>
2024-09-06 08:41:04 +0700

    EP.7

--- EP.6/homework.ts
+++ EP.6/homework.ts
@@ -1,13 +1,13 @@
-const personalInformation = {
-    fullName: 'นายสมปอง สนองบุญ',
-    address: '123/4',
-    moo: '5',
-    road: 'ลื่นไหล',
-    Subdistrict: 'น้ำตก',
-    district: 'อัมพรรณ',
-    province: 'หัวร้อน',
-    zipCode: '11101001',
-    telePhone: '0*7654321',
-    phone: '9876543*21',
-    email: 'timestone@f.uc',
-}
+// const personalInformation = {
+//     fullName: 'นายสมปอง สนองบุญ',
+//     address: '123/4',
+//     moo: '5',
+//     road: 'ลื่นไหล',
+//     Subdistrict: 'น้ำตก',
+//     district: 'อัมพรรณ',
+//     province: 'หัวร้อน',
+//     zipCode: '11101001',
+//     telePhone: '0*7654321',
+//     phone: '9876543*21',
+//     email: 'timestone@f.uc'
+// }
---
+++ EP.7/index.ts
@@ -0,0 +1,16 @@
+//JavaScript Objects
+const personalInformation = {
+    fullName: 'นายสมปอง สนองบุญ',
+    address: '123/4',
+    moo: '5',
+    road: 'ลื่นไหล',
+    Subdistrict: 'น้ำตก',
+    district: 'อัมพรรณ',
+    province: 'หัวร้อน',
+    zipCode: '11101001',
+    telePhone: '0*7654321',
+    phone: '9876543*21',
+    email: 'timestone@f.uc'
+}
+personalInformation.fullName = 'สวัสดี' //เปลี่ยนข้อมูลในตัวแปร fullName
+console.log(personalInformation.fullName)
